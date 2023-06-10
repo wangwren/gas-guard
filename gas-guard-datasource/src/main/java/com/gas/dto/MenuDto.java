abc0f8b2a6861f16ca1ac6da5a3499e7ecaae26f
wangwren <wangwren0906@gmail.com>
2023-06-10 19:34:50 +0800

    菜单增加前端渲染路径

--- gas-guard-datasource/src/main/java/com/gas/dto/MenuDto.java
+++ gas-guard-datasource/src/main/java/com/gas/dto/MenuDto.java
@@ -14,4 +14,9 @@ public class MenuDto extends TreeNode<MenuDto> implements Serializable {
     private Integer parentId;
 
     private String name;
+
+    //前端渲染路径，配置在数据库中
+    private String path;
+
+    private String icon;
 }
